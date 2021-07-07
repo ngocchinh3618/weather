3270ca5957320bd03a79f1e96c1dad0d6ae470cd
Chinh Huynh <chinh.huynh@s3corp.com.vn>
2021-07-07 17:26:19 +0700

    change name fn, add localStore, add fn getE

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-const userAction = async (lat = 10.775238, lon = 106.632524) => {
-  
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4ad98682883ede3d088095d90aaf6a65`);
-  const myJson = await response.json();
+let day = document.getElementById("day");
+let city = document.getElementById("city");
+let wind = document.getElementById("wind");
+let tempp = document.getElementById("temp");
+let humidity = document.getElementById("humidity");
+let preciptation = document.getElementById("preciptation");
 
-  const response1 = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&q=${myJson.name}&appid=4ad98682883ede3d088095d90aaf6a65`);
-  const myJson1 = await response1.json();
-  console.log(myJson); 
-  console.log(myJson1); 
+const userAction = async (lat, lon) => {
+  const getAPILocation =  await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4ad98682883ede3d088095d90aaf6a65`);
+  const getAPILocationJson =  await getAPILocation.json();
+  // console.log('result',getAPILocation, getAPILocationJson)
+  const getAPIName = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&q=${getAPILocationJson.name}&appid=4ad98682883ede3d088095d90aaf6a65`);
+  const getAPINameJson = await getAPIName.json();
+  console.log(getAPINameJson)
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   document.getElementById("time-zone").innerHTML = "Time Zone: " + timezone;
-  document.getElementById("day").innerHTML= myJson1.list[0].dt_txt;
-  document.getElementById("city").innerHTML= myJson1.city.name;
-  document.getElementById("preciptation").innerHTML= myJson1.list[0].rain["3h"];
-  document.getElementById("humidity").innerHTML= myJson1.list[0].wind.speed;
-  document.getElementById("wind").innerHTML= myJson1.list[0].main.humidity;
-  document.getElementById("temp").innerHTML= Math.round((myJson1.list[0].main.temp) - 273);
-  myFunction(myJson1.list[0].dt, "dow");
+  day.innerHTML= (getAPINameJson.list[0].dt_txt).split(' ')[0];
+  city.innerHTML= getAPINameJson.city.name;
+  humidity.innerHTML= getAPINameJson.list[0].wind.speed;
+  wind.innerHTML= getAPINameJson.list[0].main.humidity;
+  tempp.innerHTML= Math.round((getAPINameJson.list[0].main.temp) - 273);
+  getDow(getAPINameJson.list[0].dt, "dow");
+
+  const keys = Object.keys(getAPINameJson.list[0]);
   
+  for (let i = 0; i < keys.length; i++) {
+    // console.log('keys', keys[i])
+    if( keys[i] == "rain"){
+      preciptation.innerHTML= getAPINameJson.list[0].rain["3h"];
+    }
+    else{
+      preciptation.innerHTML= 0;
+    }
+  }
+
   let count = 0;
   for (let i = 1; i < 40; i++) {
     if(i % 8 == 0){
       count++;
-      var asd = myJson1.list[i].dt;
-      var temp = Math.round((myJson1.list[i].main.temp) - 273);
+      var asd = getAPINameJson.list[i].dt;
+      var temp = Math.round((getAPINameJson.list[i].main.temp) - 273);
       console.log(temp)
-      myFunction(asd, "item"+count);
+      getDow(asd, "item"+count);
       temperature(temp,"temp"+count);
     }
   }
-
 }
-
+function setE(id, value) {
+  document.getElementById(id).innerHtml = value;
+}
 function getCurrentLocation(){
   navigator.geolocation.getCurrentPosition((pos)=> {
     var crd = pos.coords;
@@ -38,21 +55,19 @@ function getCurrentLocation(){
   }, (err)=>{
     alert("An error happened!!! " + err.message);
   })
-  
 }
 function changeLocation(){
   let iLatitude = document.getElementById("iLat").value; 
   let longitude = document.getElementById("iLon").value;
-  if(iLatitude == 0 || longitude == 0 ){
+  if(iLatitude == undefined || longitude == undefined ){
     userAction()
   }
   else{
     userAction(iLatitude,longitude);
   }
-  console.log("iLatitude"+ iLatitude);
 }
 
-function myFunction(dt, tag) {
+function getDow(dt, tag) {
   var d = new Date(dt * 1000);
   var n = d.getDay()
   switch (n) {
@@ -85,152 +100,21 @@ let temperature = (temp, tag)=>{
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  userAction();
+  const position = JSON.parse(window.localStorage.getItem('location'));
+  console.log(position)
+  if(!position.long|| !position.lat){
+
+  }else{
+    userAction(position.lat,position.long);
+  }
+  
 });
-  
 
+// call a rest web service api
+// https://stackoverflow.com/questions/36975619/how-to-call-a-rest-web-service-api-from-javascript
 
+// get api by city name
+// https://openweathermap.org/current
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-// const userAction = async () => {
-//   const response = await fetch('https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyAUWrHZbX_SmR2TehFKl9xfzXv6dTFNXSM', {
-//     method: 'POST',
-//     body: myBody, // string or object
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   });
-//   const myJson = await response.json(); //extract JSON from the http response
-//   // do something with myJson
-// }
-
-// const userAction = async () => {
-//   const response = await fetch('https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810,-119.6822510&timestamp=1331161200&key=AIzaSyAUWrHZbX_SmR2TehFKl9xfzXv6dTFNXSM');
-//   const myJson = await response.json(); //extract JSON from the http response
-//   console.log(myJson);
-//   // do something with myJson
-// }
-
-// // Note: This example requires that you consent to location sharing when
-// // prompted by your browser. If you see the error "The Geolocation service
-// // failed.", it means you probably did not give permission for the browser to
-// // locate you.
-
-// console.log(object)
-
-
-// let map, infoWindow;
-
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: -34.397, lng: 150.644 },
-//     zoom: 6,
-//   });
-//   infoWindow = new google.maps.InfoWindow();
-//   const locationButton = document.createElement("button");
-//   locationButton.textContent = "My location";
-//   locationButton.classList.add("custom-map-control-button");
-//   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-//   locationButton.addEventListener("click", () => {
-//     // Try HTML5 geolocation.
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           const pos = {
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude,
-//           };
-//           console.log(pos)
-//           infoWindow.setPosition(pos);
-//           infoWindow.setContent("Location found.");
-//           infoWindow.open(map);
-//           map.setCenter(pos);
-//         },
-//         () => {
-//           handleLocationError(true, infoWindow, map.getCenter());
-//         }
-//       );
-//     } else {
-//       // Browser doesn't support Geolocation
-//       handleLocationError(false, infoWindow, map.getCenter());
-//     }
-//   });
-// }
-
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(
-//     browserHasGeolocation
-//       ? "Error: The Geolocation service failed."
-//       : "Error: Your browser doesn't support geolocation."
-//   );
-//   infoWindow.open(map);
-// }
-
-// document.addEventListener("DOMContentLoaded", function(event) { 
-//   initMap();
-// });
-
-
-
-// function seconds_with_leading_zeros(dt) 
-// { 
-//   return /\((.*)\)/.exec(new Date().toString())[1];
-// }
-
-// dt = new Date(); 
-// console.log(seconds_with_leading_zeros(dt)); 
-
-// dt = new Date(1989, 10, 1); 
-// console.log(seconds_with_leading_zeros(dt));
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   userAction();
- 
-//   // var today = new Date();
-//   // var date = today.getFullYear()+'-'+("0" + (today.getMonth() + 1)).slice(-2)+'-'+
-//   // ("0" + today.getDate()).slice(-2);
-//   // var time = ("0" + today.getHours()).slice(-2) + ":" + ("0" + today.getMinutes()).slice(-2) + ":" + ("0" + today.getSeconds()).slice(-2);
-//   // var dateTime = date+' '+time;
-  
-//   // myFunction(today.getTime()/1000, dateTime);
-
-  
-    
-// });
+// get api by cBy geographic coordinates
+// https://openweathermap.org/current#geo
